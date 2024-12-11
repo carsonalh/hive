eece5f5a80b86e59fca522a65ce19a52fb58b982
Carson Holloway <carsonalh@gmail.com>
2024-12-11 15:16:53 +1000

    Upgrade to react router v7

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router";
 import {App} from "./components/App";
 import GameplayLocal from "./components/GameplayLocal";
 import GameplayOnline from "./components/GameplayOnline";
